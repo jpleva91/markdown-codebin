3433b64c5ab4a01abc09cec5e03f105883a43d48
Jared Pleva <jpleva91@gmail.com>
2017-12-21 15:16:55 -0700

    adds 'bins.share' meteor method to onShareClick in BinsShare component

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class BinsShare extends Component {
+  onShareClick() {
+    const email = this.refs.email.value;
+    Meteor.call('bins.share', this.props.bin, email);
+  }
+
   render() {
     return (
       <footer className="bins-share">
         <div className="input-group">
-          <input className="form-control" />
+          <input ref="email" className="form-control" />
           <div className="input-group-btn">
-            <button className="btn btn-default">
+            <button
+              onClick={this.onShareClick.bind(this)}
+              className="btn btn-default">
               Share Bin
             </button>
           </div>
