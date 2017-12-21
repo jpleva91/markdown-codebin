f0ba5016a22832528904d61289d8895d200428ac
Jared Pleva <jpleva91@gmail.com>
2017-12-21 13:55:38 -0700

    adds router links to header and bin list items

--- client/components/app.js
+++ client/components/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './header';
-import BinsList from './bins/bins_list';
 
-export default () => {
+import Header from './header';
+
+export default (props) => {
   return (
     <div>
       <Header />
-      <BinsList />
+      {props.children}
     </div>
   );
-}
+};
