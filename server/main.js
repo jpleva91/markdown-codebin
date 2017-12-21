403c73bc608a1bbeba7512aba750b7d546b6271f
Jared Pleva <jpleva91@gmail.com>
2017-12-21 15:32:02 -0700

    adds 'sharedBins' publication to server

--- server/main.js
+++ server/main.js
@@ -7,6 +7,14 @@ Meteor.startup(() => {
   });
 
   Meteor.publish('sharedBins', function() {
+    const user = Meteor.users.findOne(this.userId);
 
+    if (!user) { return; }
+
+    const email = user.emails[0].address;
+
+    return Bins.find({
+      sharedWith: { $elemMatch: { $eq: email }}
+    });
   });
 });
