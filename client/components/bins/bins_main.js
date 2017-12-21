835e9c0a46abcf8a67480daf9e8b31a248e07470
Jared Pleva <jpleva91@gmail.com>
2017-12-21 14:18:05 -0700

    adds bins_editor component to bins_main

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
 
 class BinsMain extends Component {
   render() {
-    return(
-      <div>BinsMain</div>
+    return (
+      <div>
+        <BinsEditor bin={this.props.bin} />
+      </div>
     );
   }
 }
 
-export default createContainer(() => {
+export default createContainer((props) => {
+  const { binId } = props.params;
+  Meteor.subscribe('bins');
 
+  return { bin: Bins.findOne(binId) };
 }, BinsMain);
