b90ec0c238e5a3224560b1f61e8ef6eb1f9ad0de
Jared Pleva <jpleva91@gmail.com>
2017-12-21 14:55:49 -0700

    working on bins_share component

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -4,7 +4,14 @@ class BinsShare extends Component {
   render() {
     return (
       <footer className="bins-share">
-        BinsShare
+        <div className="input-group">
+          <input className="form-control" />
+          <div className="input-group-btn">
+            <button className="btn btn-default">
+              Share Bin
+            </button>
+          </div>
+        </div>
       </footer>
     );
   }
